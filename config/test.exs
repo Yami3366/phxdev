d592d4250fb1385d7a777fa27d43218c51ed2e2d
SungYam (Yammy) <159643271+Yami3366@users.noreply.github.com>
2024-05-27 17:50:10 +0100

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :phxdev, Phxdev.Repo,
-  username: "backend_stuff",
-  password: "Abcd1234",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost",
   database: "phxdev_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
